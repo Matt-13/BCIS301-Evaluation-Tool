34cf1fb8359a6cd3f36e55981ca6f0d4195d7dec
Matthew Whitaker <matthew130713@gmail.com>
2019-05-04 12:12:30 +1200

    Project Success works correctly now.

--- js/calc.js
+++ js/calc.js
@@ -19,30 +19,18 @@ const eff_mgnt = 14.51;
 const eff_bdgt = 12.06;
 const in_stk_in = 13.10;
 const in_usr_in = 11.83;
-const in_stf_tr = 3.91;
+const in_stf_tr = 3.92;
 
 // Array Items
 
 failure_elements = {
-    "tmi": top_mgmt,
-    "pmi": prj_mgmt,
-    "ssi": stf_skil,
-    "pfi": prc_frmk,
-    "thi": tch_impl,
-    "ozi": org_govt,
-    "uui": usr_usec,
-    "cxi": com_size
+    "tmi": top_mgmt, "pmi": prj_mgmt, "ssi": stf_skil, "pfi": prc_frmk,
+    "thi": tch_impl, "ozi": org_govt, "uui": usr_usec, "cxi": com_size
 }
 
 success_elements = {
-    "eci": eff_comm,
-    "ifi": imp_fwrk,
-    "osi": org_skls,
-    "emi": eff_mgnt,
-    "iti": in_stf_tr,
-    "ebi": eff_bdgt,
-    "isii": in_stk_in,
-    "iuii": in_usr_in
+    "eci": eff_comm, "ifi": imp_fwrk, "osi": org_skls, "emi": eff_mgnt,
+    "iti": in_stf_tr, "ebi": eff_bdgt, "isii": in_stk_in, "iuii": in_usr_in
 }
 
 failure_chance = 0;
@@ -54,8 +42,13 @@ function calc() {
     for(var i in (failure_elements)) {
         failure_chance += failure_elements[i] * (document.getElementById(i).value / 10);
     }
+    for(var i in (success_elements)) {
+        success_chance += success_elements[i] * (document.getElementById(i).value / 10);
+    }
     failure_chance = parseFloat(failure_chance).toFixed(2);
+    success_chance = parseFloat(success_chance).toFixed(2);
     console.log(failure_chance);
+    console.log(success_chance);
 }
 
 function calcStart() {
