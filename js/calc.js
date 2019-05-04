f35dec440fb6aebcd1fef14f0e0c670e595c1769
Matthew Whitaker <matthew130713@gmail.com>
2019-05-04 19:09:33 +1200

    Updated Code Quality, also updated constants to reflect the actual Gaps%

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,23 @@
 // Failure Factor Constants
 
-const top_mgmt = 24.55;
-const prj_mgmt = 16.17;
-const stf_skil = 16.17;
-const usr_usec = 13.17;
-const tch_impl = 11.98;
-const prc_frmk = 7.78;
-const org_govt = 6.59;
-const com_size = 3.59;
+const prj_mgmt = 31.60;
+const top_mgmt = 21.67;
+const prc_frmk = 14.22;
+const stf_skil = 9.93;
+const usr_usec = 9.48;
+const tch_impl = 7;
+const com_size = 3.16;
+const org_govt = 2.93;
 
 
 // Success Factor Constants
 
 const eff_comm = 15.65;
 const imp_fwrk = 15.42;
-const org_skls = 13.51;
 const eff_mgnt = 14.51;
-const eff_bdgt = 12.06;
+const org_skls = 13.51;
 const in_stk_in = 13.10;
+const eff_bdgt = 12.06;
 const in_usr_in = 11.83;
 const in_stf_tr = 3.92;
 
@@ -47,35 +47,41 @@ function calc() {
 }
 
 function outputToScreen() {
-    document.getElementById("failure_output").textContent = `Your Project has a ${failure_chance}% chance of Failure`
-    document.getElementById("success_output").textContent = `Your Project has a ${success_chance}% chance of Success`
+    document.getElementById("failure_output").textContent = `Your Project has a ${failure_chance}% chance of Failure`;
+    document.getElementById("success_output").textContent = `Your Project has a ${success_chance}% chance of Success`;
 }
+
+offender = '';
+value = '';
 
 function calcStart() {
     if (checkIfValuesPresent()) {
-        calc()
-        outputToScreen()
+        calc();
+        outputToScreen();
     } else {
-        window.alert("Values are Missing or Invalid in the textboxes.\nPlease double check your input.")
+        window.alert(`Values are Missing or Invalid in the textboxes.
+                    \nPlease double check your input.\nPlease check the: ${offender}
+                    \nThe input value: '${value}' is not valid.
+                    \nPlease input a number from 0 to 10.`);
     }
 }
 
 function checkIfValuesPresent() {
     for(var i in failure_elements) {
         // Bad Day Scenario
-        if(document.getElementById(i).value == "" || document.getElementById(i).value == null) {
-            return false;
-        }
-        if(document.getElementById(i).value < 0 || document.getElementById(i).value > 10) {
+        if(document.getElementById(i).value == "" || document.getElementById(i).value == null ||
+            document.getElementById(i).value < 0 || document.getElementById(i).value > 10) {
+            offender = document.getElementById(i).getAttribute("name");
+            value = document.getElementById(i).value.toString();
             return false;
         }
     }
     for(var i in success_elements) {
         // Bad Day Scenario
-        if(document.getElementById(i).value == "" || document.getElementById(i).value == null) {
-            return false;
-        }
-        if(document.getElementById(i).value < 0 || document.getElementById(i).value > 10) {
+        if(document.getElementById(i).value == "" || document.getElementById(i).value == null ||
+            document.getElementById(i).value < 0 || document.getElementById(i).value > 10) {
+            offender = document.getElementById(i).getAttribute("name");
+            value = document.getElementById(i).value.toString();
             return false;
         }
     }
@@ -85,10 +91,10 @@ function checkIfValuesPresent() {
 
 function randomize() {
     for(var i in (failure_elements)) {
-        document.getElementById(i).value = getRandomInt(10)
+        document.getElementById(i).value = getRandomInt(10);
     }
     for(var i in (success_elements)) {
-        document.getElementById(i).value = getRandomInt(10)
+        document.getElementById(i).value = getRandomInt(10);
     }
 }
 
